4ad0bb5b396caeaad26a5cc6cdd01b0ca14266b0
Bùi Chí Nguyên <135578858+skynguyenlee@users.noreply.github.com>
2025-09-16 22:03:17 +0700

    Test

---
+++ cataloghome-service/src/routes/index.js
@@ -0,0 +1,11 @@
+
+const CategoryRouter = require("./CategoryRouter");
+const ProductRouter = require("./ProductRouter");
+
+const routes = (app) => {
+
+    app.use("/api", CategoryRouter);
+    app.use("/api", ProductRouter);
+};
+
+module.exports = routes;
