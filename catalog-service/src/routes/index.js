9f01ff6fe9bb72de4332c684bc9ac3a75a1ae726
Bùi Chí Nguyên <135578858+skynguyenlee@users.noreply.github.com>
2025-09-20 15:19:48 +0700

    Nguyen/category-product/update code

--- catalog-service/src/routes/index.js
+++ catalog-service/src/routes/index.js
@@ -1,11 +1,8 @@
 
-const AuthRouter = require("./AuthRouter");
 const CategoryRouter = require("./CategoryRouter");
 const ProductRouter = require("./ProductRouter");
 
 const routes = (app) => {
-
-    app.use("", AuthRouter);
     app.use("/api", CategoryRouter);
     app.use("/api", ProductRouter);
 };
